895ece74bc736cc17625553c24a6d07938b9e47a
Olivier <olivierdautricourt@gmail.com>
2015-06-08 04:49:08 +0200

    recaptcha.go

---
+++ recaptcha.go
@@ -0,0 +1,41 @@
+package recaptcha
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+type Response struct {
+	success    bool
+	errorCodes []string
+}
+
+var api_url = "https://www.google.com/recaptcha/api/siteverify"
+
+var privatekey string
+
+func SetPrivateKey(key string) {
+	privatekey = key
+}
+
+func CheckResponse(response string) bool {
+	var r Response
+	resp, _ := http.PostForm(api_url,
+		url.Values{"secret": {privatekey}, "response": {response}})
+	decoder := json.NewDecoder(resp.Body)
+	jsonErr := decoder.Decode(&r)
+	if jsonErr != nil {
+		fmt.Printf("Couldnt decode http response into JSON")
+	} else {
+		if len(r.errorCodes) > 0 {
+			fmt.Printf("Received error codes from google api :")
+			for _, v := range r.errorCodes {
+				fmt.Printf(v)
+			}
+		}
+		return r.success
+	}
+	return false
+}
